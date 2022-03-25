98252e83f743a5a8c7ceac9f520a0a6d12a231d9
Max Mongkalakorn <sm4791@columbia.edu>
2022-03-25 13:09:43 -0400

    renamed main to quantregCF

---
+++ src/quantregCF-py/quantregCF.py
@@ -0,0 +1,17 @@
+from loaddata import loaddata
+from first_reg import first_stage_reg
+from second_reg import second_stage_reg
+from dev_lambda import dev_lambda
+from asympt_variance import asympt_variance
+
+def quantregCF(option=1, degree=3, tau_first_stage=0.5 , tau_second_stage=0.5, data_type=0, data_source):
+    # load the dataset
+    dep_var, endog_var, exog_var, z_var, dim_z = loaddata(data_type, data_source)
+
+    # regressions
+    vhat = first_stage_reg(endog_var, z_var, dim_z, tau_first_stage)
+    w_hat, beta, second_stage_res = second_stage_reg(dep_var, endog_var, exog_var, tau_second_stage, degree, option, vhat)
+    derivative_lambda = dev_lambda(exog_var, beta, option, degree, vhat)
+    se = asympt_variance(exog_var, z_var, tau_second_stage, w_hat, vhat, second_stage_res, derivative_lambda)
+
+    return beta, se
