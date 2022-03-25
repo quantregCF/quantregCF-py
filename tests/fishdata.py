7532149f07352ac03b037955026b43c05e57ea9e
Max Mongkalakorn <sm4791@columbia.edu>
2022-03-25 14:07:34 -0400

    fix test file

--- tests/fishdata.py
+++ tests/fishdata.py
@@ -6,7 +6,7 @@ var_lst = ['qty', 'price', ["day1", "day2", "day3", "day4"], ["stormy", "mixed"]
 data_lst = [0, data_source, var_lst]
 
 # regressions
-beta, se = quantregCF(option=1, degree=3, tau_first_stage=0.5, tau_second_stage=0.5, data=data_lst):
+beta, se = quantregCF(option=1, degree=3, tau_first_stage=0.5, tau_second_stage=0.5, data=data_lst)
 
 # CI
 betahat = beta.value[0]
@@ -19,6 +19,6 @@ ci_ub = betahat + 1.96 * se[0]
 # Quantile:  0.5 0.5 Estimate:  -0.3229033782770835  Standard Error:  0.344419675456656
 # 95% Confidence interval:  -0.9979659421721293 0.35215918561796233
 
-print("Quantile: ", tau_first_stage, tau_second_stage, "Estimate: ", betahat, " Standard Error: ", se[0])
+print("Quantile: 0.5 0.5", "Estimate: ", betahat, " Standard Error: ", se[0])
 print("95% Confidence interval: ", ci_lb, ci_ub)
 
