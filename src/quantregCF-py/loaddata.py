08b54f4c907d9365152e18df5b8d3f7d72be2409
Max Mongkalakorn <sm4791@columbia.edu>
2022-03-25 13:44:17 -0400

    loaddata can now take any dataset

--- src/quantregCF-py/loaddata.py
+++ src/quantregCF-py/loaddata.py
@@ -1,25 +1,25 @@
 from urllib.request import urlopen
 import pandas as pd
 
-def loaddata(data_type, data_source):
+def loaddata(data_type, data_source, variable_lst):
     if data_type == 0:
         url = urlopen(data_source)
         df = pd.read_stata(url)
 
+    dep_var_name = variable_lst[0]
+    endog_var_name = variable_lst[1]
+    exog_var_list = variable_lst[2]
+    iv_var_list = variable_lst[3]
+
     # define the dependent variable (Y) and the endogenous right-hand side variable (X)
-    dep_var = df["qty"].to_numpy()
-    endog_var = df["price"].to_numpy()
+    dep_var = df[dep_var_name].to_numpy()
+    endog_var = df[endog_var_name].to_numpy()
 
     # define exogenous included variables
-    exog_var_list = ["day1", "day2", "day3", "day4"]
     exog_var = df[exog_var_list].to_numpy()
 
-    # define instrumental variables
-    #iv_var_list = ["stormy", "mixed"]
-    #iv_var = df[iv_var_list].to_numpy()
-
     # define all exogenous variables
-    z_var_list = ["day1", "day2", "day3", "day4", "stormy", "mixed"]
+    z_var_list = exog_var_list + iv_var_list
     z_var = df[z_var_list].to_numpy()
 
     dim_z = len(z_var_list)
